4adbbb216cb26ec2eaa523df4346abdb7c44bf74
fahad <43818190+fsq2@users.noreply.github.com>
2022-09-15 11:36:57 +0300

    finish steps for basic_cleaning, testing and eda

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -5,6 +5,7 @@ An example of a step using MLflow and Weights & Biases
 import argparse
 import logging
 import wandb
+import pandas as pd 
 
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
@@ -13,44 +14,78 @@ logger = logging.getLogger()
 
 def go(args):
 
-    run = wandb.init(job_type="basic_cleaning")
+    run = wandb.init(project="nyc_airbnb", job_type="basic_cleaning")
     run.config.update(args)
 
     # Download input artifact. This will also log that this script is using this
     # particular version of the artifact
-    # artifact_local_path = run.use_artifact(args.input_artifact).file()
-
-    ######################
-    # YOUR CODE HERE     #
-    ######################
-
+    logger.info("Downloading artifact")
+    artifact_local_path = run.use_artifact(args.input_artifact).file()
+    df = pd.read_csv(artifact_local_path)
+    
+    logger.info("Set Max & Min For Price Col")
+    min_price = args.min_price
+    max_price = args.max_price
+    idx = df['price'].between(min_price, max_price)
+    df = df[idx].copy()
+    
+    logger.info("Fix Date For Last Reveiw Col")
+    df['last_review'] = pd.to_datetime(df['last_review'])
+    
+    df.to_csv("clean_sample.csv", index=False)
+    
+    artifact = wandb.Artifact(
+     args.output_artifact,
+     type=args.output_type,
+     description=args.output_description,)
+  
+    artifact.add_file("clean_sample.csv")
+    run.log_artifact(artifact)
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="This steps cleans the data")
 
-
     parser.add_argument(
-        "--parameter1", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
+        "--input_artifact",
+        type=str,
+        help="Name of the input artifact in W&D",
+        required=True       
+    )
+    parser.add_argument(
+        "--output_artifact",
+        type=str,
+        help=" Name of the output ",
+        required=True
+    )
+    
+    parser.add_argument(
+        "--output_type",
+        type=str,
+        help=" Type of the output",
+        required=True
+    )
+    
+    parser.add_argument(
+        "--output_description",
+        type=str,
+        help="Output describtion",
+        required=True
+    )
+    
+    parser.add_argument(
+        "--min_price", 
+        type=float,
+        help="Min value for the price column",
         required=True
     )
 
     parser.add_argument(
-        "--parameter2", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
+        "--max_price", 
+        type=float,
+        help="Max value for the price column",
         required=True
     )
-
-    parser.add_argument(
-        "--parameter3", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
-        required=True
-    )
-
 
     args = parser.parse_args()
 
